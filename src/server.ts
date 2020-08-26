07db8c7db37a7fc65c0556ad9cee6771676781db
henriquepe <henrique.p.espindola@gmail.com>
2020-08-26 19:41:38 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
+
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes/index';
 import './database';
@@ -7,6 +9,9 @@ import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
